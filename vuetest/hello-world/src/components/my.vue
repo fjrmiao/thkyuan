985c7ea7a7cd0e674a129ba4de0e83549cdc6af8
fjrmiao <48702502+fjrmiao@users.noreply.github.com>
2020-12-10 15:42:50 +0800

    fixerror

--- vuetest/hello-world/src/components/my.vue
+++ vuetest/hello-world/src/components/my.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="my">
-	<h2>{{title}}</h2>
-	<div v-on:click="x=x+1;message=my[x%2];">{{ message }}</div>
-  </div>
+    <div id="my">
+        <h2>{{ title }}</h2>
+        <div @click="x=x+1;message=my[x%2];">{{ message }}</div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'My',
-  data() {
-        return{
-                x:0,
-            	my:['有朋自远方来','好人一生平安'],
-            	title: '名言',
-              message: '有朋自远方来'
+    name: 'My',
+    data() {
+        return {
+            x: 0,
+            my: ['有朋自远方来', '好人一生平安'],
+            title: '名言',
+            message: '有朋自远方来'
         }
 
-  }
+    }
 }
 </script>
 
--- vuetest/hello-world/src/main.js
+++ vuetest/hello-world/src/main.js
@@ -1,22 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import App from './App.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 import My from '@/components/my.vue'
 
 Vue.use(Router)
 
-export default new Router({
-    routes:[
-    {
-        path:'/',
-        name:'App',
-        component: App
-    },
-    {
-        path:'/my',
-        component: My
-    }
+const router = new Router({
+    routes: [
+        {path: '/', component: HelloWorld},
+        {path: '/my', component: My}
     ]
 })
 
+// 脚手架 vue-cli 3.0 差异
+// vue有两种形式的代码 compiler（模板）模式和runtime模式（运行时），vue模块的package.json的main字段默认为runtime模式， 指向了"dist/vue.runtime.common.js"位置
+// import Vue from 'vue/dist/vue.esm.js'
+//
+// new Vue({
+//     el: '#app',
+//     router: router,
+//     template: '<App/>',
+//     components: { App }
+// })
 
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount("#app")
